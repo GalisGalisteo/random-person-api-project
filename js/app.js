40af95ed364112050a9acdc480dac7172e57e082
GalisGalisteo <galis.travelling@gmail.com>
2023-11-02 20:04:09 +0100

    finishing ex without bonus

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-// Tu código aquí
+const url = 'https://randomuser.me/api/';
+
+const fetchPokemon = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error('Error fetching the pokemon');
+    }
+    const data = await response.json();
+    return data;
+}
+
+
+document.querySelector('#btn').addEventListener('click', async () => {
+    const person = await fetchPokemon(url);
+    const firstName = person.results[0].name.first;
+    const lastName = person.results[0].name.last;
+    const country = person.results[0].location.country;
+    const phone = person.results[0].cell;
+    const email = person.results[0].email;
+    const picture = person.results[0].picture.large;
+
+    document.querySelector('#first').textContent = firstName;
+    document.querySelector('#last').textContent = lastName;
+    document.querySelector('#country').textContent = country;
+    document.querySelector('#phone').textContent = phone;
+    document.querySelector('#email').textContent = email;
+    document.querySelector('#photo').src = picture;
+})
